0b9dd6cd83e9ac7d7f353c11f18b24f2eae21262
sneakersspicy03-svg <admin@spicy.com>
2026-03-24 00:15:24 -0400

    fix: base url to relative path v5.3 for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/agenda-virtual/', 
+  base: './', // Cambiamos a rutas relativas para máxima compatibilidad
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets',
   }
 });
