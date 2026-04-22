c01de7faa6e46ad2f1bcef31b14918a00ce81659
sneakersspicy03-svg <admin@spicy.com>
2026-04-21 21:29:52 -0400

    fix: integracion de credenciales limpias de agenda-virtual-v2

--- firebase/config.ts
+++ firebase/config.ts
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNfOqPVjxnCwR35fGGWrn36p8HHw-ZePM",
-  authDomain: "agenda-virtual-48e4e.firebaseapp.com",
-  projectId: "agenda-virtual-48e4e",
-  storageBucket: "agenda-virtual-48e4e.firebasestorage.app",
-  messagingSenderId: "937291727034",
-  appId: "1:937291727034:web:bad8557b864e3de6190283"
+  apiKey: "AIzaSyBMEIOCeYCV4qI2bJldOqcGawxNCeZtyZo",
+  authDomain: "agenda-virtual-v2.firebaseapp.com",
+  projectId: "agenda-virtual-v2",
+  storageBucket: "agenda-virtual-v2.firebasestorage.app",
+  messagingSenderId: "534197561949",
+  appId: "1:534197561949:web:5c9ec9c5e48642ea27bbb4",
+  measurementId: "G-LM3JB4M9Y3"
 };
 
 export const app = initializeApp(firebaseConfig);
